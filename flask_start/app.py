214f00b8a89419c41fe7f6a3379731bd7599ba5f
chengmingzhang <18216642724@163.com>
2019-06-05 13:24:13 +0800

    flask - 01

--- flask_start/app.py
+++ flask_start/app.py
@@ -13,9 +13,15 @@ def setting():
     return 'Setting Page'
 
 
-@app.route('/profile/')
+@app.route('/profile/', methods=['GET', 'POST'])
 def profile():
-    return render_template('profile.html')  # 模板渲染，图像传递
+    # return render_template('profile.html')  # 模板渲染，图像传递
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        phone = request.form.get('telphone')
+        password = request.form.get('password')
+        return render_template('profile.html')
 
 
 @app.route('/parameter/pass/')
@@ -23,6 +29,7 @@ def parameter_pass():
     name = request.args.get('name')
     age = request.args.get('age')
     return '%s, %s' % (name, age)
+
 
 
 if __name__ == '__main__':
